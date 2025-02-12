b00534c2761a132f11cb0a19538b6d61d9cab6ce
plinscho <paul.linschoten@gmail.com>
2025-02-12 16:20:25 +0100

    modified main

--- module07/ex00/main.cpp
+++ module07/ex00/main.cpp
@@ -1,24 +1,17 @@
 #include "Whatever.hpp"
 
-template <typename T>
-void testSwap(T a, T b) {
-	std::cout << "BEFORE SWAP:\t" << std::endl;
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	::swap(a, b);
-	std::cout << "AFTER SWAP:\t" << std::endl;
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
-}
-
-int main() {
+int main( void ) {
 	int a = 2;
 	int b = 3;
-	testSwap(a, b);
-
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
-	testSwap(c, d);
-
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return 0;
 }
